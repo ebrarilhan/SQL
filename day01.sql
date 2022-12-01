10acfa9d3e7d4612944e35c31453081946388d22
ebrarilhan <salihaebrarilhan@gmail.com>
2022-12-01 22:29:40 +0100

    01/12/22

---
+++ day01.sql
@@ -0,0 +1,48 @@
+--database (veritabani) olusturma
+create database ebrar;
+
+
+--DDL - DATA DEFINITION LANGUAGE
+-- CREATE - TABLO OLUSTUMA 
+--buyuk kucuk harf fark etmiyor.
+
+CREATE TABLE ogrenciler2
+(
+ogrenci_no char(7), 
+isim varchar(20),
+soyisim varchar(25),  
+not_ort real,  --ondalikli sayilar icin kullanilir. double gibi
+kayit_tarih date
+)
+
+
+--var olan tablodan yeni bir tablo olusturma:
+
+create table ogrenci_notlari
+AS --benzer tablodaki basliklarla ve data tipleriyle yeni bir tablo olusturmak icin 
+--normal tablo olustururkenki parantezler yerine AS kullanip select komutuyla almak istedigimiz verileri aliriz.
+select isim, soyisim, not_ort from ogrenciler2;
+
+
+-- DML - DATA MANIPULATION LANGUAGE
+-- INSERT database'e veri ekleme
+
+INSERT INTO ogrenciler2 values ('1234567','Said','ILHAN',85.5,now());
+INSERT INTO ogrenciler2 values ('1234567','Said','ILHAN',85.5,'2020-12-11');
+
+
+--bir tabloya parcali veri eklemek istersek
+
+insert into ogrenciler2 (isim,soyisim) values ('Erol','Evren');
+
+
+-- DQL - DATA QUERY LANG.
+--SELECT
+
+select * from ogrenciler2; 
+
+
+
+
+
+
